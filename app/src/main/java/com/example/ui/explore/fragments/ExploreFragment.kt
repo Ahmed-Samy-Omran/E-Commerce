9d145fc8bfebb624661afe6a4c34779ad274c39a
Omran77 <96256555+Omran77@users.noreply.github.com>
2025-02-15 21:51:52 +0200

    add home features and connect come navigation with fragment by viewPager

---
+++ app/src/main/java/com/example/ui/explore/fragments/ExploreFragment.kt
@@ -0,0 +1,31 @@
+package com.example.ui.explore.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.e_commerce.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ExploreFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ExploreFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+
+}
--- app/src/main/java/com/example/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/ui/home/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +12,14 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.example.e_commerce.R
 import com.example.e_commerce.databinding.ActivityMainBinding
+import com.example.ui.account.fragments.AccountFragment
 import com.example.ui.common.viewmodel.UserViewModel
 import com.example.ui.auth.AuthActivity
+import com.example.ui.cart.fragments.CartFragment
+import com.example.ui.home.adapter.HomeViewPagerAdapter
+import com.example.ui.explore.fragments.ExploreFragment
+import com.example.ui.home.fragments.HomeFragment
+import com.example.ui.offers.fragments.OffersFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -24,7 +29,7 @@ import kotlinx.coroutines.runBlocking
 class MainActivity : AppCompatActivity() {
     private val userViewModel: UserViewModel by viewModels()
 
-    private var _binding:ActivityMainBinding ?= null
+    private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +48,49 @@ class MainActivity : AppCompatActivity() {
 
 
         initViewModel()
+        intViews()
+
+
+    }
+
+    private fun intViews() {
+        initViewPager()
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeFragment -> binding.homeViewPager.currentItem = 0
+                R.id.exploreFragment -> binding.homeViewPager.currentItem = 1
+                R.id.cartFragment -> binding.homeViewPager.currentItem = 2
+                R.id.offerFragment -> binding.homeViewPager.currentItem = 3
+                R.id.accountFragment -> binding.homeViewPager.currentItem = 4
+            }
+            true
+        }
+    }
+    private fun initViewPager() {
+        val fragments = listOf(
+            //make sure the order of fragments is the same as the order of the bottom navigation view items
+            HomeFragment(),
+            ExploreFragment(),
+            CartFragment(),
+            OffersFragment(),
+            AccountFragment()
+
+        )
+//        binding.homeViewPager.offscreenPageLimit = fragments.size // that make all screen created and loaded at once and not necessary to open it to load
+        binding.homeViewPager.adapter = HomeViewPagerAdapter(this, fragments)
+      // that make when i swipe the viewpager the bottom navigation view change the selected item
+        binding.homeViewPager.registerOnPageChangeCallback(
+            object : androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomNavigationView.menu.getItem(position).isChecked = true
+                }
+            }
+        )
     }
 
     private fun initViewModel() {
@@ -56,7 +104,6 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
-
 
 
     override fun onResume() {
